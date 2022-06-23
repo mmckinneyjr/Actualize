fd0b297fa988b77fc8bdb40ccbb2ac885a7b87db
Mark Mckinney <mmckinneyjr@gmail.com>
2022-06-23 01:14:19 -0500

    Completed the 01Grades Deep Dive

--- independent-practice/w01/01_grades/grades.rb
+++ independent-practice/w01/01_grades/grades.rb
@@ -1,6 +1,26 @@
 def final_grade(array)
+  sum = 0
 
-  # This method currently has no code.
-  # Your job is to write the code to get the method to work!
+  array.each do |g|
+    sum = sum + g
+  end
 
+  if array.size == 0
+    "I"
+  else
+    num_grade = (sum / array.size).round()
+    puts num_grade
+
+    if num_grade >= 90
+      "A"
+    elsif num_grade >= 80 && num_grade < 90
+      "B"
+    elsif num_grade >= 70 && num_grade < 80
+      "C"
+    elsif num_grade >= 60 && num_grade < 80
+      "D"
+    else
+      "F"
+    end
+  end
 end

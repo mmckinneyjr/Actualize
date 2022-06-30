f08c6fb778b730e7d6226710eefb036e13a3d7a4
Mark Mckinney <mmckinneyjr@gmail.com>
2022-06-30 12:19:59 -0500

    added data to db

--- week02-practice/mini-capstone/mini-capstone-api/db/seeds.rb
+++ week02-practice/mini-capstone/mini-capstone-api/db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(name: "iPhone", price: 900, image_url: "https://images.unsplash.com/photo-1525598912003-663126343e1f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGhvbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60", description: "The iPhone is a smartphone made by Apple that combines a computer, iPod, digital camera and cellular phone into one device with a touchscreen interface.")
+product.save
+product = Product.new(name: "keyboard", price: 30, image_url: "https://images.unsplash.com/photo-1587829741301-dc798b83add3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=865&q=80", description: "A computer keyboard is an input device used to enter characters and functions into the computer system by pressing buttons, or keys. It is the primary device used to enter text. A keyboard typically contains keys for individual letters, numbers and special characters")
+product.save
+product = Product.new(name: "iron man", price: 50, image_url: "https://images.unsplash.com/photo-1608278047522-58806a6ac85b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YWN0aW9uJTIwZmlndXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60", description: "Armored Avenger - driven by a heart that is part machine, but all hero! He is the INVINCIBLE IRON MAN! Iron Man's Powers and Abilities: Wears modular arc reactor-powered Iron Man armor, granting superhuman strength & durability, the ability to fly & project Repulsor blasts.")
+product.save
+product = Product.new(name: "deadpool", price: 55, image_url: "https://images.unsplash.com/photo-1608889335941-32ac5f2041b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YWN0aW9uJTIwZmlndXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60", description: "Deadpool, whose real name is Wade Winston Wilson, is a disfigured mercenary with the superhuman ability of regeneration and physical prowess. The character is known as the Merc with a Mouth because of his tendency to talk and joke constantly, including breaking the fourth wall for humorous effect and running gags.")
+product.save
+product = Product.new(name: "stormtrooper", price: 10, image_url: "https://images.unsplash.com/photo-1608889825271-9696283ab804?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGFjdGlvbiUyMGZpZ3VyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60", description: "Stormtroopers are elite shock troops fanatically loyal to the Empire and impossible to sway from the Imperial cause. They wear imposing white armor, which offers a wide range of survival equipment and temperature controls to allow the soldiers to survive in almost any environment.")
+product.save
+product = Product.new(name: "junk", price: 20, image_url: "https://images.unsplash.com/photo-1609041958269-fc44467d925d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c3R1ZmZ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60", description: "Junk is defined as garbage, trash or worthless items, talk or ideas, or to bonds that are unlikely to be paid back to the investor, or is slang for a narcotic drug. Crumpled up papers and old toys are an example of junk. Silly reality television shows with no merit to them are an example of junk.")
+product.save
+product = Product.new(name: "game", price: 5, image_url: "https://images.unsplash.com/photo-1611996575749-79a3a250f948?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2FtZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60", description: "a structured form of play, usually undertaken for entertainment or fun, and sometimes used as an educational tool. Games are different from work, which is usually carried out for remuneration, and from art, which is more often an expression of aesthetic or ideological elements.")
+product.save

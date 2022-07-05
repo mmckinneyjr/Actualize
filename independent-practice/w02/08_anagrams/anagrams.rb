3afc29294314d841900a5dfc0bb3e6cc5f749cf4
Mark Mckinney <mmckinneyjr@gmail.com>
2022-07-04 21:19:03 -0500

    week02 day05 deep dive - anagrams

--- independent-practice/w02/08_anagrams/anagrams.rb
+++ independent-practice/w02/08_anagrams/anagrams.rb
@@ -1,3 +1,13 @@
 def match(word, list)
-  # Your job is to complete the code for this method!
+  anagram_list = []
+
+  list.each do |list_word|
+    if list_word.downcase.split("").sort == word.downcase.split("").sort && list_word.downcase != word.downcase
+      anagram_list << list_word
+    end
+  end
+  return anagram_list
+
 end
+
+

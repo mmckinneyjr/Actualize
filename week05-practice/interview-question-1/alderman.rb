564f86ccee4bc2155aa98a0aea8840622aa52cf7
Mark Mckinney <mmckinneyjr@gmail.com>
2022-07-25 09:53:04 -0500

    added menu and search functionality

--- week05-practice/interview-question-1/alderman.rb
+++ week05-practice/interview-question-1/alderman.rb
@@ -2,20 +2,21 @@ class Alderman
   attr_reader :ward, :name, :email, :phone, :zip
 
   def initialize(input_options)
+    @name = input_options[:name] || "No info provided"
     @ward = input_options[:ward] || "No info provided"
-    @first_name = first_name(input_options[:name]) || "No info provided"
-    @last_name = last_name(input_options[:name]) || "No info provided"
+    @first_name = first_name() || "No info provided"
+    @last_name = last_name() || "No info provided"
     @email = input_options[:email] || "No info provided"
     @phone = input_options[:phone] || "No info provided"
     @zip = input_options[:zip] || "No info provided"
   end
 
-  def first_name(name)
-    name.nil? ? "No info provided" : name.split(",")[1]
+  def first_name()
+    @name.nil? ? "No info provided" : @name.split(",")[1]
   end
 
-  def last_name(name)
-    name.nil? ? "No info provided" : name.split(",")[0]
+  def last_name()
+    @name.nil? ? "No info provided" : @name.split(",")[0]
   end
 
   def print_alderman
--- week05-practice/interview-question-1/graffiti-removal-requests.rb
+++ week05-practice/interview-question-1/graffiti-removal-requests.rb
@@ -2,6 +2,9 @@ require "http"
 require "./alderman"
 require "./removal_request"
 
+@aldermans = []
+@requests = []
+
 def get_wards
   ward_offices = HTTP.get("https://data.cityofchicago.org/resource/htai-wnw4.json")
 
@@ -13,8 +16,7 @@ def get_wards
       phone: office["ward_phone"],
       zip: office["zipcode"],
     )
-
-    alderman.print_alderman
+    @aldermans << alderman
   end
 end
 
@@ -32,9 +34,98 @@ def get_requests
       street_address: request["address"],
       police_district: request["police_district"],
     )
-
-    removal_request.print_request
+    @requests << removal_request
   end
 end
 
 get_wards()
+get_requests()
+
+while true
+  puts "-----------------------------------------"
+  puts "   Chicago's Graffiti Removal Requests"
+  puts "-----------------------------------------"
+  puts
+  puts "   [1] Alderman's last name"
+  puts "   [2] Month and year"
+  puts "   [3] Print all Alderman"
+  puts "   [4] Print all requests"
+  puts "   [5] To exit program"
+  puts
+  print "Search by: "
+
+  search_by = gets.chomp
+
+  if search_by.to_i == 1
+    print "Enter an Alderman's last name: "
+    search_alderman = gets.chomp
+    puts
+
+    found_alderman = @aldermans.each_index.select { |i| @aldermans[i].last_name.downcase == search_alderman.downcase }
+
+    if found_alderman.empty? == false
+      found_alderman.each do |f|
+        puts
+        puts "-----------------------------------------"
+        @aldermans[f].print_alderman
+        puts "-----------------------------------------"
+        puts
+        found_requests = @requests.each_index.select { |i| @requests[i].ward == @aldermans[f].ward }
+        found_requests.each do |r|
+          @requests[r].print_request
+        end
+      end
+    else
+      system("clear")
+      puts "-------- NO SEARCH RESULTS FOUND --------"
+    end
+    puts
+
+    ########################################
+  elsif search_by.to_i == 2
+    print "Enter a month: "
+    search_month = gets.chomp.to_i
+    if search_month.between?(1, 12)
+      print "Enter a year: "
+      search_year = gets.chomp.to_i
+
+      found_searches = @requests.each_index.select { |i|
+        ((@requests[i].creation_date)[5..6].to_i == search_month) &&
+        ((@requests[i].creation_date)[0..3].to_i == search_year)
+      }
+      if found_searches.empty? == false
+        found_searches.each do |r|
+          @requests[r].print_request
+        end
+      else
+        system("clear")
+        puts "--------- NO SEACH RESULTS FOUND---------"
+        puts
+      end
+    else
+      system("clear")
+      puts "------------- INVALID MONTH -------------"
+      puts
+    end
+
+    ########################################
+  elsif search_by.to_i == 3
+    puts
+    @aldermans.each do |x|
+      x.print_alderman
+    end
+    ########################################
+  elsif search_by.to_i == 4
+    puts
+    @requests.each do |x|
+      x.print_request
+    end
+    ########################################
+  elsif search_by.to_i == 5
+    exit
+  else
+    system("clear")
+    puts
+    puts "#{search_by} is an invalid entry, try again"
+  end
+end
